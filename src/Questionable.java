2e6701cc54f802c01cca88ffb8ebee7ab426f63a
Maisc-fm <maisarah.cedric@gmail.com>
2026-05-29 23:00:34 +0800

    Update Questionable.java

--- src/Questionable.java
+++ src/Questionable.java
@@ -1,3 +1,13 @@
-public class Questionable {
+//Interface: Questionable
+//Creator：Bong Ming Meng (103541)
+//Defines the contract for all question types in the Quality Education Quiz.
+
+public interface Questionable {
+
+    String getQuestionText();
+    boolean checkAnswer(String userAnswer);
+    String getCorrectAnswer();
+    String getQuestionType();
+    int getPoints();
     
 }
