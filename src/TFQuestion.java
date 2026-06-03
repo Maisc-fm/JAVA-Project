d1d638ac8857192288235cd292675afd21838240
nashrur <aisyhahani123@gmail.com>
2026-06-03 21:33:56 +0800

    tf question update 2

--- src/TFQuestion.java
+++ src/TFQuestion.java
@@ -1,86 +1,42 @@
-import java.util.Arrays;
-import java.util.List;
+// Class: TFQuestion
+// Represents a True/False question.
+// Demonstrates:
+// - Interface Implementation
+// - Polymorphism
+// - Method Overriding
+// Created By: Nashrur Aisyha Hani binti Suphian @ Sharbini (102776)
 
-// TFQuestion (True/False Question)
-// Made by Nashrur Aisyha Hani binti Suphian @ Sharbini (102776) 
-// This class represents a True/False quiz question.
-// It stores the question, the correct answer and checks whether the user's answer is correct.
 public class TFQuestion implements Questionable {
 
-    // Stores the correct answer (true = True, false = False)
+    private String question;
     private boolean answer;
 
-    // Stores the question text
-    private String question;
-
-    // Creates a new True/False question.
-    // @param question The question to display
-    // @param answer The correct answer (true or false)
+    // Constructor
+    // @param question Question text
+    // @param answer Correct answer
     public TFQuestion(String question, boolean answer) {
         this.question = question;
         this.answer = answer;
     }
 
-    // Returns the question text.
+    // Returns question text.
     @Override
     public String getQuestion() {
         return question;
     }
 
-    // Checks if the user's answer is correct.
-    // Accepts either a Boolean value or a String. 
+    // Checks answer.
     @Override
-    public boolean checkAnswer(Object a) {
+    public boolean checkAnswer(String userAnswer) {
 
-        // If the answer is a Boolean, compare it directly
-        if (a instanceof Boolean) {
-            return (Boolean) a == answer;
-        }
+        boolean userBoolean =
+                Boolean.parseBoolean(userAnswer);
 
-        // If the answer is a String, convert it to true/false first
-        if (a instanceof String) {
-            boolean parsed = Boolean.parseBoolean(((String) a).trim());
-            return parsed == answer;
-        }
-
-        // Any other type of input is considered incorrect
-        return false;
+        return userBoolean == answer;
     }
 
-    // Returns the question type.
-    @Override
+    //Returns question type.
     public String getType() {
-        return "TF";
-    }
-
-    // Returns the available answer choices.
-    @Override
-    public List<String> getOptions() {
-        return Arrays.asList("True", "False");
-    }
-
-    // Returns the correct answer.
-    public boolean getAnswer() {
-        return answer;
-    }
-
-    // Updates the correct answer.
-    public void setAnswer(boolean answer) {
-        this.answer = answer;
-    }
-
-    // Updates the question text.
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    // Returns the object information as a String.
-    // Useful for testing and debugging.
-    @Override
-    public String toString() {
-        return "TFQuestion{" +
-               "question='" + question + '\'' +
-               ", answer=" + answer +
-               '}';
+        return "True/False Question";
     }
 }
