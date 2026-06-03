db4380493adb1c9b23a6b940333909920944dbf9
nashrur <aisyhahani123@gmail.com>
2026-06-03 21:27:51 +0800

    displayable update

--- src/Displayable.java
+++ src/Displayable.java
@@ -1,3 +1,21 @@
-public class Displayable {
-    
+// Interface: Displayable
+// Purpose: Defines the methods that any learning content class must implement in order to display educational pages and related images.
+// Created By: Nashrur Aisyha Hani binti Suphian @ Sharbini (102776)
+public interface Displayable {
+
+    // Displays a specific learning page.
+    // @param index the page number to display
+    void showPage(int index);
+
+    // Returns the total number of learning pages.
+    // @return total number of pages
+    int getTotalPages();
+
+    // Returns the title of the learning module.
+    // @return learning module title
+    String getTitle();
+
+    // Returns the image path associated with the current page.
+    // @return image file path
+    String getImagePath();
 }
